8729b93752654c084c1f3b2e445fc4c6a6b4b882
thePeras <jtopereira@gmail.com>
2023-12-21 16:39:49 +0000

    Package setup

---
+++ example/up_menus_example.dart
@@ -0,0 +1,6 @@
+import 'package:up_menus/up_menus.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/up_menus.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library up_menus;
+
+export 'src/up_menus_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
